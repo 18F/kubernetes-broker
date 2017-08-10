5773afc524aea87b1b3c039d63967a5f964f06e2
Roger Steve Ruiz <roger.ruiz@gsa.gov>
2017-08-10 13:27:54 -0400

    Add preStop hook for Server;
    This triggers a manual failover whenever a Master Redis server
    shutsdown.

---
+++ custom_images/redis-3.2/server/pre-stop.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+set -eux
+
+primary_server_ip_from_sentinel=$(redis-cli --raw -h $SENTINEL_HOST -p $SENTINEL_PORT SENTINEL get-master-addr-by-name primaryserver | head -1)
+
+primary_server_ip_from_self=$(redis-cli --raw -a $REDIS_PASSWORD CONFIG GET slave-announce-ip | tail -1)
+
+if [[ $primary_server_ip_from_self == $primary_server_ip_from_sentinel ]]
+then
+  echo "Shutting down master, manual failover on primaryserver."
+  redis-cli -h $SENTINEL_HOST -p $SENTINEL_PORT -c "SENTINEL failover primaryserver"
+fi
