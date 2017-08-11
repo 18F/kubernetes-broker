9e39d785bd97f366054e16353822652732e84d77
Roger Steve Ruiz <roger.ruiz@gsa.gov>
2017-08-11 01:17:55 -0400

    Make sure redis can write to conf along with root

--- custom_images/redis-3.2/server/run.sh
+++ custom_images/redis-3.2/server/run.sh
@@ -6,3 +6,5 @@ mkdir -p /opt/bin
 cp /pre-stop.sh /dig-a /dig-srv /k8s-redis-ha-server /opt/bin
 cp /redis.template.conf /opt
 chmod -R +x /opt/bin
+chmod -R g=+w /opt/redis.conf /opt/redis.template.conf
+chown redis /opt/redis.conf /opt/redis.template.conf
