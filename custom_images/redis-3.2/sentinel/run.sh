216526a13baa990ab6c1b0ce17f61218cd03a282
Roger Steve Ruiz <roger.ruiz@gsa.gov>
2017-08-16 15:06:26 -0400

    Really fix the Sentinel Init images;
    h/t @cnelson

--- custom_images/redis-3.2/sentinel/run.sh
+++ custom_images/redis-3.2/sentinel/run.sh
@@ -2,8 +2,10 @@
 
 set -eux
 
-apt-get update && apt-get install -y netcat
+
 mkdir -p /opt/bin
-cp /dig-a /dig-srv /k8s-redis-ha-sentinel /opt/bin
+
+cp /k8s-redis-ha-sentinel /opt/bin
 cp /sentinel.template.conf /opt
+
 chmod -R +x /opt/bin
