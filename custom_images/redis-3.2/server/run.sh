41ad0ed85eca4ba515c0629e04420a2ffb8a8818
Chris Nelson <christopher.nelson@gsa.gov>
2017-08-16 13:53:54 -0700

    WIP. One container to rule them all; no init containers

--- custom_images/redis-3.2/server/run.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-set -eux
-
-mkdir -p /opt/bin
-cp /pre-stop.sh /dig-a /dig-srv /k8s-redis-ha-server /opt/bin
-cp /redis.template.conf /opt
-chmod -R +x /opt/bin
-chmod -R g=+w /opt/redis.template.conf
